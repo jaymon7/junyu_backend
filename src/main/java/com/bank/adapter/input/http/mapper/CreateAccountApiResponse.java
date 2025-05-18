793292d0e5a5241c7e0456efb2b77653e44d72b4
Jun Yu <yujun93@naver.com>
2025-05-19 01:08:14 +0900

    update: openapi response 수정

--- src/main/java/com/bank/adapter/input/http/mapper/CreateAccountApiResponse.java
+++ src/main/java/com/bank/adapter/input/http/mapper/CreateAccountApiResponse.java
@@ -1,9 +1,22 @@
 package com.bank.adapter.input.http.mapper;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record CreateAccountApiResponse(
-        String accountId,
+        @Schema(
+                description = "계좌 소유자 이름",
+                example = "홍길동"
+        )
         String accountHolderName,
+        @Schema(
+                description = "계좌번호",
+                example = "123-456-7890"
+        )
         String accountNumber,
+        @Schema(
+                description = "계좌 잔액",
+                example = "10000"
+        )
         Integer balance
 ) {
 }
